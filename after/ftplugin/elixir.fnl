df2e0c6a36285587584d5382872a8992265995e1
Isaac Spencer <22ispencer@gmail.com>
2025-05-29 06:57:07 -0500

    elixir

---
+++ after/ftplugin/elixir.fnl
@@ -0,0 +1,4 @@
+(set vim.o.shiftwidth 2)
+(set vim.o.tabstop 2)
+(set vim.o.expandtab true)
+(vim.lsp.enable "elixirls")
---
+++ after/ftplugin/elixir.lua
@@ -0,0 +1,5 @@
+-- [nfnl] after/ftplugin/elixir.fnl
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.expandtab = true
+return vim.lsp.enable("elixirls")
